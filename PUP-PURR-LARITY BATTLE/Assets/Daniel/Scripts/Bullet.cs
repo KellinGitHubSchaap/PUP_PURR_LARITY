f42328035f136be5994091839e1c63e4256930b4
Lavisch <65831893+Lavisch@users.noreply.github.com>
2022-02-23 13:41:13 +0100

    Added functionality for shooting satellites + spawning satellites randomly

---
+++ PUP-PURR-LARITY BATTLE/Assets/Daniel/Scripts/Bullet.cs
@@ -0,0 +1,62 @@
+using System;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private float lifeTime;
+    [SerializeField] private SpriteRenderer sprite;
+    
+
+    private float _lifeTimer;
+    private bool _canHit = true;
+    private Transform _transform;
+    private BoxCollider2D _collider;
+    private void Awake()
+    {
+        _collider = GetComponent<BoxCollider2D>();
+        _transform = transform;
+    }
+    
+    public void Init(Vector2 position)
+    {
+        _collider.enabled = false;
+        _canHit = true;
+        _lifeTimer = 0;
+        _transform.localScale = Vector3.one;
+        _transform.localPosition = position;
+        sprite.sortingOrder = 1;
+    }
+
+    private void Update()
+    {
+        if (_lifeTimer < lifeTime)
+        {
+            _lifeTimer += Time.deltaTime;
+            _transform.localScale -= Vector3.one / lifeTime * Time.deltaTime;
+            
+            if (_lifeTimer > lifeTime / 2 && _canHit)
+            {
+                _collider.enabled = true;
+                Invoke(nameof(GoPast), 0.05f);
+            }
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void GoPast()
+    {
+        _canHit = false;
+        sprite.sortingOrder = -1;
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (!col.CompareTag("Satellite") || !_canHit) return;
+        
+        col.GetComponent<Satellite>().Hit();
+        gameObject.SetActive(false);
+    }
+}
