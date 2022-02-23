984e8ea824a36a9c0e4e8449df8168250e9d42e4
Lavisch <65831893+Lavisch@users.noreply.github.com>
2022-02-23 14:44:06 +0100

    small cleanups

---
+++ PUP-PURR-LARITY BATTLE/Assets/Daniel/Scripts/CharacterMovement.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public enum CharacterFacing {Left, Right}
+public class CharacterMovement : MonoBehaviour
+{
+    public void MoveTo(Vector2 position)
+    {
+        
+    }
+}
