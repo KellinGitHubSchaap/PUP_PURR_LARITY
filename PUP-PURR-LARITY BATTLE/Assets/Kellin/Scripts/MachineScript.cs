820b508435699b017cbf8f46b593229f69ed7e38
KellinSchaap <200557@student.glu.nl>
2022-02-24 13:22:47 +0100

    Machinery Braking Down and Fixing Set up

--- PUP-PURR-LARITY BATTLE/Assets/Kellin/Scripts/MachineScript.cs
+++ PUP-PURR-LARITY BATTLE/Assets/Kellin/Scripts/MachineScript.cs
@@ -8,6 +8,8 @@ public class MachineScript : MonoBehaviour
     public bool m_isFixed = false;
     public bool m_isGettingFixed = false;
 
+    public GameObject m_exclamationPoint;
+
     private void OnMouseDown()
     {
         if (!m_isFixed && m_movementScript.m_isFocused)
@@ -22,6 +24,14 @@ public class MachineScript : MonoBehaviour
         StartCoroutine(FixMachine());
     }
 
+    public void BreakDown()
+    {
+        m_isFixed = false;
+        m_exclamationPoint.SetActive(true);
+        tag = "Needs Fixing";
+        GetComponent<MeshRenderer>().material = WorldScript.instance.m_machineBrokeMaterial;
+    }
+
     private IEnumerator FixMachine()
     {
         yield return new WaitForSeconds(3);
@@ -32,5 +42,7 @@ public class MachineScript : MonoBehaviour
         m_isGettingFixed = false;
 
         m_movementScript.m_catState = BodyMovementScript.CatState.Wandering;
+
+        m_exclamationPoint.SetActive(false);
     }
 }
